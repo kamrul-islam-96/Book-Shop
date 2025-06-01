9567ad8acb4e11ccd5a22968492452b96f4d5fa8
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-06-01 20:05:21 +0600

    Hero Image Issue Fix

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -3,7 +3,7 @@ import '../Style/HeroBanner.css'
 function HeroBanner() {
   return (
     <div className="hero">
-      <img src="../public/Images/Hero Image.jpg" alt="HSC & Honours Books" />
+      <img src="Images/Hero Image.jpg" alt="HSC & Honours Books" />
       <div className="hero-text">
         <h1>HSC ও Honours বই</h1>
         <p>পুরাতন বই কিনুন অর্ধেক দামে!</p>
